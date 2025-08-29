2c8f44304aed2e1225e0c5886c2663b9f3c716a9
luisalbertodr <129796147+luisalbertodr@users.noreply.github.com>
2025-08-29 18:53:53 +0200

    Update appwrite.ts

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -5,7 +5,7 @@ import { Client, Account, Databases, Storage, ID } from 'appwrite';
 const client = new Client();
 
 client
-  .setEndpoint('https://212.230.23.11:4443/v1')
+  .setEndpoint('https://appwrite.lipoout.com:4443/v1')
   .setProject('68a3408b002f26b39ccd');
 
 // Servicios de Appwrite
